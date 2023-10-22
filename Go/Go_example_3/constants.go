d269a6e675919491a2f4b6c9a5deb25b676e4ddb
Neonx <fabiangomes2015@gmail.com>
2023-10-22 18:44:31 -0500

    variable creada e inicialozada de  manera abreviada

--- Go/Go_example_3/constants.go
+++ Go/Go_example_3/constants.go
@@ -15,6 +15,19 @@ func main() {
 
 	fmt.Println(reflect.TypeOf(Myvar))
 
+	var num int = 2
+
+	var myFloat float32 = 6.2
+	println(myFloat + float32(num))
+
+	var myBool bool = true
+	println(myBool)
+
+	//variable creada e inicialozada de  manera abreviada
+
+	myString := "texto"
+	print(myString)
+
 	// Myvar = 2 error
 }
 
