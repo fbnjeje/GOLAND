064cdb9050ab5848541252f14e8a699c8debb3e5
Neonx <fabiangomes2015@gmail.com>
2023-10-22 18:18:22 -0500

    cambio

--- Go/Go_example_3/constants.go
+++ Go/Go_example_3/constants.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
-const s string = "constants"
-
 func main() {
 
-	fmt.Print("hola como estas")
+	var Myvar string = "Esto se una cadena de texto"
+	fmt.Println("hola como estas ", Myvar)
+
+	Myvar = "Esto se modifica Gente"
+	fmt.Println(Myvar)
 }
 
 //esto es un comentario JAJAJAJAJ
