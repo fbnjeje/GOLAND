fe45742b9ae2203c4611aee6292634a35dfc01d4
Neonx <fabiangomes2015@gmail.com>
2023-10-22 18:13:08 -0500

    comentarios

---
+++ Go/Go_example_3/constants.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const s string = "constants"
+
+func main() {
+
+	fmt.Print("hola como estas")
+}
+
+//esto es un comentario JAJAJAJAJ
+
+/*
+
+esto es un comentario largo
+*/
