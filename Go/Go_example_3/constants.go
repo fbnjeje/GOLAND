307891f770ae3a768cdc32f4f3984b3dce3ddc1e
Neonx <fabiangomes2015@gmail.com>
2023-10-22 19:02:42 -0500

    array

--- Go/Go_example_3/constants.go
+++ Go/Go_example_3/constants.go
@@ -2,46 +2,53 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 
-	var Myvar string = "Esto se una cadena de texto"
-	fmt.Println("hola como estas ", Myvar)
+	// var Myvar string = "Esto se una cadena de texto"
+	// fmt.Println("hola como estas ", Myvar)
 
-	Myvar = "Esto se modifica Gente"
-	fmt.Println(Myvar)
+	// Myvar = "Esto se modifica Gente"
+	// fmt.Println(Myvar)
 
-	fmt.Println(reflect.TypeOf(Myvar))
+	// fmt.Println(reflect.TypeOf(Myvar))
 
-	var num int = 5
+	// var num int = 5
 
-	// var myFloat float32 = 6.2
-	// println(myFloat + float32(num))
+	// // var myFloat float32 = 6.2
+	// // println(myFloat + float32(num))
 
-	var myBool bool = true
-	println(myBool)
+	// var myBool bool = true
+	// println(myBool)
 
-	//variable creada e inicialozada de  manera abreviada
+	// //variable creada e inicialozada de  manera abreviada
 
-	myString := "texto"
-	print(myString)
+	// myString := "texto"
+	// print(myString)
 
 	// const myConst = "esto es una constante"
 
 	// control de flujo jaja
 
-	num = 2
-	myString = "hola"
+	// num := 2
+	// myString := "hola"
 
-	if num == 2 && myString == "hola" {
-		fmt.Print("el valor es 2")
-	} else if num == 5 {
-		fmt.Print("el valor es 5")
-	} else {
-		fmt.Print("el valor no es 2")
-	}
+	// if num == 2 && myString == "hola" {
+	// 	fmt.Print("el valor es 2")
+	// } else if num == 5 {
+	// 	fmt.Print("el valor es 5")
+	// } else {
+	// 	fmt.Print("el valor no es 2")
+	// }
+
+	var myArray [3]int
+
+	myArray[0] = 0
+	myArray[1] = 1
+	myArray[2] = 2
+
+	fmt.Println(myArray)
 
 	// Myvar = 2 error
 }
