e8be51791a506e990e40b28469f7e3604762e86a
Neonx <fabiangomes2015@gmail.com>
2023-10-22 22:37:19 -0500

    menu y scanner del dato ingresado

--- Go/Todo_Proyect/to_do.go
+++ Go/Todo_Proyect/to_do.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type task struct {
+	name        string
+	description string
+	completed   bool
+}
 
 func main() {
 
-	fmt.Print("hola")
+	tasks := make(map[int]task)
+
+	lastId := 0
+
+	for {
+		fmt.Println("Selecciona  la opcion deseada:")
+		fmt.Println("1. ver tareas")
+		fmt.Println("2. agregar tareas")
+		fmt.Println("3. Marcar tarea como completada")
+		fmt.Println("4. Eliminar tarea")
+		fmt.Println("5. Salir")
+
+		fmt.Print("opcion: ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+	}
 }
