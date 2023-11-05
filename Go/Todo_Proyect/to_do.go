2ecd1628dbdf14287d976b7fd1d6ea2392056946
David Hernandez <davidfelipehernandez1@gmail.com>
2023-11-05 12:00:43 -0500

    update test

--- Go/Todo_Proyect/to_do.go
+++ Go/Todo_Proyect/to_do.go
@@ -20,12 +20,6 @@ func main() {
 	lastId := 0
 
 	for {
-		fmt.Println("Selecciona  la opcion deseada:")
-		fmt.Println("1. ver tareas")
-		fmt.Println("2. agregar tareas")
-		fmt.Println("3. Marcar tarea como completada")
-		fmt.Println("4. Eliminar tarea")
-		fmt.Println("5. Salir")
 
 		fmt.Print("Opcion: ")
 
